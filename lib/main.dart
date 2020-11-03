00dd30d32ca1c8f6fc5323e4e74fe9ddcdeadab9
Ziad Al-Moqamar <ziad.almoqamar@gmail.com>
2020-11-03 18:01:48 +0200

    making themedata

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,10 @@ class BMICalculator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData.dark().copyWith(
+        primaryColor: Color(0xFF0A0D22),
+        scaffoldBackgroundColor: Color(0xFF0A0D22),
+      ),
       home: InputPage(),
     );
   }
@@ -25,9 +29,6 @@ class _InputPageState extends State<InputPage> {
       ),
       body: Center(
         child: Text('Body Text'),
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
       ),
     );
   }
